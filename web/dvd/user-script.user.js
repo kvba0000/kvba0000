5bf711a09575e0934c508207aa58bca98e1eda62
@kob.kuba <47297843+kbd0t@users.noreply.github.com>
2023-08-15 21:51:45 +0200

    fix for brave :smiley: :sob:

--- web/dvd/user-script.user.js
+++ web/dvd/user-script.user.js
@@ -15,6 +15,14 @@
 
     unsafeWindow.USERSCRIPT_LOADED = true;
 
+    const moveWindow = (x,y) => {
+        if(typeof navigator.brave === 'undefined') window.moveTo(x,y)
+        else { // fix for brave - f*ck you brave <3
+            window.moveBy(-999999,-999999)
+            window.moveBy(x,y)
+        }
+    }
+
     unsafeWindow.startMoving = (windowData) => {
         setInterval(() => {
             if(windowData.xx) windowData.pos.x += windowData.JUMPER;
@@ -26,7 +34,7 @@
             if(windowData.pos.x < 0+5 || windowData.pos.x > windowData.screenSize.w - windowData.size.w) {windowData.xx = !windowData.xx; hitWall()}
             if(windowData.pos.y < 0+5 || windowData.pos.y > windowData.screenSize.h - windowData.size.h - windowData.DEAD_ZONE) {windowData.yy = !windowData.yy; hitWall()}
         
-            window.moveTo(windowData.pos.x, windowData.pos.y)
+            moveWindow(windowData.pos.x, windowData.pos.y)
             window.resizeTo(windowData.size.w, windowData.size.h)
         }, windowData.FREQ);
     }
